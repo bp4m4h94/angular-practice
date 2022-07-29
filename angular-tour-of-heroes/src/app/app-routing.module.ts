fd0a606f544b3c2dd06c310c740b1f4ee6fd4d57
Ryan Tsai <bp4m4h94@gmail.com>
2022-07-29 17:46:38 +0800

    feat: Add the AppRoutingModule close #9
    1. add router module
    2. set router url path and put HeroesComponent in it
    3. add @NgModule to initialize and starts it listening for browser location changes
    4. add forRoot() method to configure router eat root level

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
 
 @NgModule({
+  // The method is called forRoot() because you configure the router at the application's root level. 
+  //The forRoot() method supplies the service providers and directives needed for routing, 
+  //and performs the initial navigation based on the current browser URL.
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
